b710230d34993c44e473354bde5c38261ade37cf
Albéric <alberic.trancart@eleves.enpc.fr>
2015-07-26 15:30:09 +0200

    Favicon

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,27 @@
 angular.module('portfolio', ['ui.router', 'ngMaterial', 'ngMessages'])
+    // Setup theme
+    .config(function($mdThemingProvider) {
+        $mdThemingProvider.definePalette('foyerPalette', {
+            '50': 'ffebee',
+            '100': 'ffcdd2',
+            '200': 'ef9a9a',
+            '300': 'e57373',
+            '400': 'ef5350',
+            '500': 'f44336',
+            '600': 'e53935',
+            '700': 'd32f2f',
+            '800': 'c62828',
+            '900': 'b71c1c',
+            'A100': 'ff8a80',
+            'A200': 'ff5252',
+            'A400': 'ff1744',
+            'A700': 'd50000',
+            'contrastDefaultColor': 'light',
+            'contrastDarkColors': ['50', '100', '200', '300', '400', 'A100'],
+            'contrastLightColors': undefined
+        });
+        $mdThemingProvider.theme('default').primaryPalette('foyerPalette');
+    })
     // Setup routes
     .config(function($stateProvider, $urlRouterProvider, $urlMatcherFactoryProvider) {
         $urlMatcherFactoryProvider.strictMode(false);
@@ -30,26 +53,37 @@ angular.module('portfolio', ['ui.router', 'ngMaterial', 'ngMessages'])
             })
         ;
     })
-    .config(function($mdThemingProvider) {
-        $mdThemingProvider.definePalette('foyerPalette', {
-            '50': 'ffebee',
-            '100': 'ffcdd2',
-            '200': 'ef9a9a',
-            '300': 'e57373',
-            '400': 'ef5350',
-            '500': 'f44336',
-            '600': 'e53935',
-            '700': 'd32f2f',
-            '800': 'c62828',
-            '900': 'b71c1c',
-            'A100': 'ff8a80',
-            'A200': 'ff5252',
-            'A400': 'ff1744',
-            'A700': 'd50000',
-            'contrastDefaultColor': 'light',
-            'contrastDarkColors': ['50', '100', '200', '300', '400', 'A100'],
-            'contrastLightColors': undefined
-        });
-        $mdThemingProvider.theme('default').primaryPalette('foyerPalette');
+    // Setup interceptor
+    .factory('ErrorCodes_Interceptor', function($rootScope, $location, $q, StorageService) {
+        return {
+            responseError: function(response) {
+                switch (response.status) {
+                case 401:
+                    StorageService.remove('token');
+                    StorageService.remove('roles');
+                    $rootScope.isLogged = false;
+                    $location.path('/');
+                    break;
+                case 403:
+                    $location.path('/403');
+                    break;
+                case 404:
+                    $location.path('/404');
+                    break;
+                case 500:
+                    $location.path('/erreur');
+                    break;
+                case 503:
+                    if (response.data.until)
+                        StorageService.set('maintenance', response.data.until);
+                    else
+                        StorageService.remove('maintenance');
+                    $location.path('/maintenance');
+                    $rootScope.maintenance = true;
+                    break;
+                }
+                return $q.reject(response);
+            }
+        };
     })
 ;
