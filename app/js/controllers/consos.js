a812796240b12a95d142c802269d331a582f571e
Albéric <alberic.trancart@eleves.enpc.fr>
2015-07-31 00:23:46 +0200

    List beers

--- app/js/controllers/consos.js
+++ app/js/controllers/consos.js
@@ -1,13 +1,29 @@
 angular.module('foyer')
-    .controller('Consos_Ctrl', function($scope) {
-
+    .controller('Consos_Ctrl', function($scope, beers) {
+        $scope.beers = beers;
+        $scope.selectedItemChange = function(item) {
+            alert(item.name);
+        };
+        $scope.searchBeer = function(item) {
+            $http
+                .post(apiPrefix + 'search', {search: '/' + string})
+                .success(function(data){
+                    $scope.beers = data;
+                })
+            ;
+        };
     })
     .config(function($stateProvider) {
         $stateProvider
             .state('root.consos', {
                 url: 'consos',
                 templateUrl: 'views/consos.html',
-                controller: 'Consos_Ctrl'
+                controller: 'Consos_Ctrl',
+                resolve: {
+                    beers: function($resource) {
+                        return $resource(apiPrefix + 'beers').query().$promise;
+                    }
+                },
             })
         ;
     })
