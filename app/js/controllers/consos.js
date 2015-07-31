509a3acb10053eb0e800401beadeb02dcf1c61fa
Albéric <alberic.trancart@eleves.enpc.fr>
2015-07-31 22:29:59 +0200

    Search users

--- app/js/controllers/consos.js
+++ app/js/controllers/consos.js
@@ -1,13 +1,29 @@
 angular.module('foyer')
-    .controller('Consos_Ctrl', function($scope, beers) {
-        $scope.beers = beers;
+    .controller('Consos_Ctrl', function($scope, $http, $q, beers, users) {
         var beer = {
             image_url: '',
             name: 'Choisis une bière',
             price: ''
         };
+        $scope.users = users;
+        $scope.beers = beers;
+        $scope.clients = [];
         $scope.beer = beer;
-        $scope.searchText = '';
+        $scope.searchText1 = '';
+        $scope.searchText2 = '';
+
+        $scope.searchUser = function(query) {
+            deferred = $q.defer();
+            var results = query ? $scope.users : $scope.users,
+            deferred;
+            $http
+                .post(apiPrefix + 'search', {search: 'User/' + query})
+                .success(function(data) {
+                    deferred.resolve(data.users);
+                })
+            ;
+            return deferred.promise;
+        };
 
         $scope.searchBeer = function(query) {
             return query ? $scope.beers.filter(createFilterFor(query)) : $scope.beers;
@@ -20,12 +36,14 @@ angular.module('foyer')
             };
         }
 
-        $scope.selectedItemChange = function(item) {
+        $scope.selectedBeerChange = function(item) {
             $scope.selectBeer(item);
         };
+
         $scope.selectBeer = function(item) {
             $scope.beer = item;
         };
+
         $scope.emptyBeer = function() {
             $scope.beer = beer;
         };
@@ -39,6 +57,9 @@ angular.module('foyer')
                 resolve: {
                     beers: function($resource) {
                         return $resource(apiPrefix + 'beers').query().$promise;
+                    },
+                    users: function($resource) {
+                        return $resource(apiPrefix + 'users').query().$promise;
                     }
                 },
             })
